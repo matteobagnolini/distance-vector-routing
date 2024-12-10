3000dcf4a7ee93daaafa15d70e796d8d3e5e7027
Matteo Bagnolini <matteobagnolini2003@gmail.com>
2024-12-10 19:16:56 +0100

    add python docs

--- network.py
+++ network.py
@@ -5,24 +5,39 @@ class Network:
         self.routers = []
 
     def add_router(self, router):
+        """Add a router to the network."""
         self.routers.append(router)
 
     def connect_routers(self, router1 : Router, router2 : Router, distance : int):
+        """Connect two routers."""
         if router1 not in self.routers or router2 not in self.routers:
             raise Exception
+
         router1.add_edge(router2.id, distance)
         router2.add_edge(router1.id, distance)
     
     # Here each router sends his informations to other routers
     def send_link_state_packets(self):
+        """
+            This function simulates the sending of link state packets from all routers, to make the
+            other routers know their connections.
+            Once this function terminates, each router has created a graph of the network.
+            
+                Note: this function does not use sequence number to enumerate the link state packets.
+        """
         for source_router in self.routers:
             for dest_router in self.routers:
                 dest_router.receive_link_state_packet(source_router)
 
     def calculate_shortest_paths(self):
+        """
+        This function calculates the shortest path for each router of the network.
+        Once this function terminates, each router's routing table has been compiled.
+        """
         for router in self.routers:
             router.calculate_shortest_path()
     
     def print_routing_tables(self):
+        """Print routing tables for each router of the network."""
         for router in self.routers:
             print("Router " + router.id + ": " + router.print_routing_table())
--- router.py
+++ router.py
@@ -3,27 +3,42 @@ import networkx as nx
 class Node:
     def __init__(self, id : str):
         self.id = id
-        self.edges : nx.Graph = nx.Graph()
+        self.adjacents : nx.Graph = nx.Graph()      # Node's adjacents are represented as a graph
 
     def add_edge(self, node_id : str, distance : int):
-        self.edges.add_edge(self.id, node_id, weight=distance)
+        self.adjacents.add_edge(self.id, node_id, weight=distance)
 
 
 class Router(Node):
+    """This class represents a router inside a network."""
     def __init__(self, id : str):
         Node.__init__(self, id)
-        self.network_graph : nx.Graph = nx.Graph()    # graph as adjacent list
-        self.routing_table : dict[str, tuple[int, str]] = {}     # dest : (cost, next hop)
+        self.network_graph : nx.Graph = nx.Graph()
+        self.routing_table : dict[str, tuple[int, str]] = {}    # dest : (cost, next hop)
     
-    def receive_link_state_packet(self, router):
-        self.network_graph = nx.compose(self.network_graph, router.edges)  # Add the edges of other graph to the current graph
+    def receive_link_state_packet(self, router : 'Router'):
+        """
+        This function simulates the receiving of a link state packet. After each
+        call of this function, the router starts building the network graph.
+        Once the router received the link state packets of all other routers,
+        network graph is completed.
+        
+        Args:
+        router (Router): The router that sent the link state packet.
+        """
+        self.network_graph = nx.compose(self.network_graph, router.adjacents)  # Current graph is composed with graph received 
 
     def calculate_shortest_path(self):
-        dist, path = nx.single_source_dijkstra(self.network_graph, self.id)
-        # Here we need to find the routing table from the pred dictionary
+        """
+        This function calculates the shortest path. After the calculation ended,
+        the routing table is compiled using the information obtained by Dijkstra
+        algorithm.
+        """
+        dist, path = nx.single_source_dijkstra(self.network_graph, self.id)     # Dijkstra algorithm is used
         for node in self.network_graph.nodes:
             if node != self.id:
-                self.routing_table[node] = (dist[node], path[node][1])
+                next_hop = path[node][1]
+                self.routing_table[node] = (dist[node], next_hop)
 
     def print_routing_table(self):
         print("----------------------------------------------")
